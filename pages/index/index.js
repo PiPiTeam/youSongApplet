b4548fba5eec3e3321994998ab61e7dc592f0821
er567 <839153198@qq.com>
2021-07-29 14:01:55 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -173,6 +173,11 @@ Page({
       }
     })
   },
+  goStudent(e) {
+    const id = e.currentTarget.dataset.id || ''
+    const url = '/pages/student/student?id='+id
+    wx.navigateTo({ url: url})
+  },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
     console.log(e)
