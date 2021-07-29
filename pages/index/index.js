5e2bde3c1e4143e79eb00897472162ad1c7417d3
costar <zhouwei520168@gmail.com>
2021-07-29 16:57:55 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -178,6 +178,12 @@ Page({
     const url = '/pages/student/student?id='+id
     wx.navigateTo({ url: url})
   },
+  viewCourses(e){
+    console.log(123333)
+    const id = e.currentTarget.dataset.id || ''
+    const url = '/pages/courses/courses?id='+id
+    wx.navigateTo({ url: url})
+  },
   getUserInfo(e) {
     // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
     console.log(e)
