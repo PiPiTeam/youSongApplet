50454b786503fbf68133176086673a677b647640
er567 <839153198@qq.com>
2021-08-01 03:33:47 +0800

    fix: 差查看更多

--- pages/events/events.js
+++ pages/events/events.js
@@ -1,19 +1,23 @@
 // pages/events/events.js
+const app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    events:['activity1@2x.png', 'activity2@2x.png', 'activity3@2x.png','activity1@2x.png', 'activity2@2x.png', 'activity3@2x.png'],
-    coursesName: ['吉他课程', '钢琴课程', '萨克斯课程', '小提琴课程', '架子鼓课程', '古筝课程', '非洲鼓课程', '流行声乐课程', '尤克里里课程' ],
+    storeId: app.globalData.storeId,
+    page: 1,
+    pages: 0,
+    eventsList:[],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 页面初次加载，请求第一页数据
+    this.getPageList(1, true)
   },
 
   /**
@@ -55,7 +59,11 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+      // 下拉触底，先判断是否有请求正在进行中
+    // 以及检查当前请求页数是不是小于数据总页数，如符合条件，则发送请求
+    if (!this.loading && this.data.page < this.data.pages) {
+      this.getInfoListData(this.data.page + 1)
+    }
   },
 
   /**
@@ -63,5 +71,31 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  goEventDetail(e) {
+    const id = e.currentTarget.dataset.id || ''
+    const url = '/pages/event-detail/event-detail?id='+id
+    wx.navigateTo({ url: url})
+  },
+  getPageList(pageNo, over) {
+    const _this = this
+    wx.request({
+      url: 'http://118.195.176.248:8002/activity/page-list',
+      method: 'GET',
+      data: {
+        storeId: _this.data.storeId,
+        current: pageNo,
+        size: 8
+      },
+      header: {
+        'Accept': 'application/json'
+      },
+      success: function(res) {
+        const resp = res.data
+        _this.setData({
+          eventsList: over ? resp.data.records : this.data.eventsList.concat(resp.data.records)
+        })
+      }
+    })
   }
 })
